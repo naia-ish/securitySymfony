6c1a7372bae0dff06eafb86efdd61f7af7b4412b
naiara <naiara@aircury.com>
2021-06-28 16:30:15 +0200

    Add Authenticator class

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -20,13 +20,20 @@ class SecurityController extends AbstractController
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
-            'error' => $error,
+            'error' => $error
         ]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
