27a2e9a0817d8d9ce850dbceaed602733e73c607
naiara <naiara@aircury.com>
2021-07-06 16:27:55 +0200

    fetch user object in the controller

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use http\Client\Curl\User;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +12,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 /**
  * @isGranted("ROLE_USER")
  */
-class AccountController extends AbstractController
+class AccountController extends BaseController
 {
     /**
      * @Route("/account", name="app_account")
      */
-    public function index(): Response
+    public function index(LoggerInterface $logger): Response
     {
+//        //nos da el objeto del user logged in
+//        dd($this->getUser());
+        $logger->debug('Checking account page for '.$this->getUser()->getEmail());
+
         return $this->render('account/index.html.twig', [
 
         ]);
