e56d1c986f1bcbe7e00bbd6069db1751b1454eb3
naiara <naiara@aircury.com>
2021-07-06 15:51:41 +0200

    Add ROLE_ADMIN role

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -27,6 +27,19 @@ class UserFixture extends BaseFixture
             ));
             return $user;
         });
+
+        $this->createMany(3, 'admin_users', function($i) {
+            $user = new User();
+            $user->setEmail(sprintf('admin%d@thespacebar.com', $i));
+            $user->setFirstName($this->faker->firstName);
+            $user->setRoles(["ROLE_ADMIN"]);
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'admin' //le ponemos a todas la misma contraseña
+            ));
+            return $user;
+        });
+
         $manager->flush();
     }
 }
