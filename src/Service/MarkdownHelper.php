256f7f5a83c71ee13053e6842177f392ec83ffa5
naiara <naiara@aircury.com>
2021-07-07 15:49:43 +0200

    Access current user object from a service with another service

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -5,6 +5,7 @@ namespace App\Service;
 use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\Security\Core\Security;
 
 class MarkdownHelper
 {
@@ -12,13 +13,15 @@ class MarkdownHelper
     private $markdown;
     private $logger;
     private $isDebug;
+    private $security;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug, Security $security)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
         $this->logger = $markdownLogger;
         $this->isDebug = $isDebug;
+        $this->security = $security;
     }
 
     public function parse(string $source): string
